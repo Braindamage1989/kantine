19f6b188bf69a6bd6ebfc2dda413279fce006f03
weetok <weetok@live.nl>
2014-11-20 09:45:16 +0100

    Verbetering setGeboortedatum

--- src/kantine/Persoon.java
+++ src/kantine/Persoon.java
@@ -24,55 +24,56 @@ public class Persoon {
     }
    
     /**
-    * Setter voor geboortedatum
-    * Met controle op juistheid van dag maand en jaar
-    */
-    public void setGeboortedatum(int tempGeboortejaar, int tempGeboortemaand, int tempGeboortedag) {
-            boolean controle=true;
-        if (tempGeboortejaar<1900 && tempGeboortejaar>2100) {
-            controle=false;
-        }
-        if (tempGeboortemaand<0 && tempGeboortemaand>12) {
-            controle=false;
-        }
-        if (tempGeboortedag<1){
-            controle=false;
-        }
-        switch(tempGeboortemaand){
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                if (tempGeboortedag>31) {
-                    controle=false;
-                }
-                break;
-            case 2:
-                if (tempGeboortedag>28) {
-                    controle=false;
-                }
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                if (tempGeboortedag>30) {
-                    controle=false;
-                }
-                break;
-        }
-        if (controle==false) {
-            tempGeboortedag=0;
-            tempGeboortemaand=0;
-            tempGeboortejaar=0;
-        }
-            geboortedag=tempGeboortedag;
-            geboortemaand=tempGeboortemaand;
-            geboortejaar=tempGeboortejaar;
-    }
+     * Setter voor geboortedatum
+     * Met controle op juistheid van dag maand en jaar
+     */
+     public void setGeboortedatum(int tempGeboortejaar, int tempGeboortemaand, int tempGeboortedag) {
+             boolean controle=true;
+         if (tempGeboortejaar<1900 || tempGeboortejaar>2100) {
+             controle=false;
+         }
+         if (tempGeboortemaand<0 || tempGeboortemaand>12) {
+             controle=false;
+         }
+         if (tempGeboortedag<1){
+             controle=false;
+         }
+         switch(tempGeboortemaand){
+             case 1:
+             case 3:
+             case 5:
+             case 7:
+             case 8:
+             case 10:
+             case 12:
+                 if (tempGeboortedag>31) {
+                     controle=false;
+                 }
+                 break;
+             case 2:
+                 if (tempGeboortedag>28) {
+                     controle=false;
+                 }
+                 break;
+             case 4:
+             case 6:
+             case 9:
+             case 11:
+                 if (tempGeboortedag>30) {
+                     controle=false;
+                 }
+                 break;
+         }
+         if (controle) {
+             geboortedag=tempGeboortedag;
+             geboortemaand=tempGeboortemaand;
+             geboortejaar=tempGeboortejaar;
+         }else{
+             geboortedag=0;
+             geboortemaand=0;
+             geboortejaar=0;
+         }
+     }
     
     public void setGeslacht(char charGeslacht) {
         if (charGeslacht=='m' || charGeslacht=='v'){
