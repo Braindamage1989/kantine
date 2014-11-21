189ac0fb85566997090a34915952cffc2b279e63
weetok <weetok@live.nl>
2014-11-21 11:47:46 +0100

    Comments in Artikel

--- src/kantine/Artikel.java
+++ src/kantine/Artikel.java
@@ -13,27 +13,43 @@ public class Artikel {
     private String naam;
     private int prijs;
     
+    /**
+     * De naam en prijs zetten als het object
+     * geconstrueerd word.
+     */
     public Artikel(String naamArtikel, int prijsArtikel) 
     {
     	naam = naamArtikel;
     	prijs = prijsArtikel;
     }
     
+    /**
+     * Zet een naam voor het artikel.
+     */
     public void setNaam(String naamArtikel)
     {
     	naam = naamArtikel;
     }
     
+    /**
+     * Zet de prijs van het artikel.
+     */
     public void setPrijs(int prijsArtikel)
     {
     	prijs = prijsArtikel;
     }
     
+    /**
+     * Return de naam van het artikel.
+     */
     public String getNaam()
     {
     	return naam;
     }
     
+    /**
+     * Return de prijs van het artikel.
+     */
     public int getPrijs()
     {
     	return prijs;
