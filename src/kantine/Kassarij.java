39571af4dcdd679f6f17c76877ad7c853361a72a
weetok <weetok@live.nl>
2014-11-28 10:09:43 +0100

    Implementatie Kassarij

--- src/kantine/Kassarij.java
+++ src/kantine/Kassarij.java
@@ -1,34 +1,54 @@
-c class KassaRij {
-  /**
-   * Constructor
-   */
-  public KassaRij() {
-    //method body omitted 
-  }
+package kantine;
 
-  /**
-   * Persoon sluit achter in de rij aan
-   * @param persoon
-   */
-  public void sluitAchteraan(Persoon persoon) {
-    //method body omitted
-  }
+import java.util.ArrayList;
 
-  /**
-   * Indien er een rij bestaat, de eerste Persoon uit
-   * de rij verwijderen en retourneren. 
-   * Als er niemand in de rij staat geeft deze null terug.
-   * @return Eerste persoon in de rij of null
-   */
-  public Persoon eerstePersoonInRij() {
-    //method body omitted 
-  }
-
-  /**
-   * Methode kijkt of er personen in de rij staan. 
-   * @return Of er wel of geen rij bestaat
-   */
-  public boolean erIsEenRij() {
-    //method body omitted
-  }
+class KassaRij {
+	private ArrayList<Persoon> personen;
+	
+	/**
+	 * Constructor
+	 */
+	public KassaRij() {
+	  personen = new ArrayList<>(); 
+	}
+	
+	/**
+	 * Persoon sluit achter in de rij aan
+	 * @param persoon
+	 */
+	public void sluitAchteraan(Persoon persoon) {
+		personen.add(persoon);
+	}
+	
+	/**
+	 * Indien er een rij bestaat, de eerste Persoon uit
+	 * de rij verwijderen en retourneren. 
+	 * Als er niemand in de rij staat geeft deze null terug.
+	 * @return Eerste persoon in de rij of null
+	 */
+	public Persoon eerstePersoonInRij() {
+		boolean eenRij = erIsEenRij();
+		if(eenRij) {
+			return personen.get(0);
+		}
+		else {
+			return null
+		}
+	}
+	
+	/**
+	 * Methode kijkt of er personen in de rij staan. 
+	 * @return Of er wel of geen rij bestaat
+	 */
+	public boolean erIsEenRij() {
+		boolean eenRij;
+		if(personen.size() > 0) {
+			eenRij = true;
+		}
+		else {
+			eenRij = false;
+		}
+		
+		return eenRij;
+	}
 }
