57f4bfc309dc39f087139bcfb15bd342e49901fd
Braindamage1989 <R.Scholten@Live.nl>
2014-11-28 15:16:57 +0100

    Kantinesimulatie gereed

--- src/kantine/KantineSimulatie_1.java
+++ src/kantine/KantineSimulatie_1.java
@@ -15,17 +15,18 @@ public class KantineSimulatie {
    */
   public void simuleer(int dagen) {
     // for lus voor dagen
-    for(...) {
+    for(int i=0;i<dagen;i++) {
       // per dag nu even vast 10+i personen naar binnen
       // laten gaan, wordt volgende week veranderd
       // for lus voor personen
       for(int j=0;j<10+i;j++){
-        // kantine.(…);
+        kantine.loopPakSluitAan();
       }
 
-      // verwerk rij voor de kassa
-      // toon dagtotalen (artikelen en geld in kassa)
-      // reset de kassa voor de volgende dag
+      kantine.verwerkRijVoorKassa();
+      kantine.aantalArtikelen();
+      kantine.hoeveelheidGeldInKassa();
+      kantine.resetKassa();
     }
   }
 }
