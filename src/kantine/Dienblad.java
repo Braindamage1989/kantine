9398a48a1078560c8ce4d191f361f72abe87f70d
Braindamage1989 <R.Scholten@Live.nl>
2014-11-28 09:04:23 +0100

    spacing toegvoegd

--- src/kantine/Dienblad.java
+++ src/kantine/Dienblad.java
@@ -3,42 +3,42 @@ package kantine;
 import java.util.ArrayList;
 
 public class Dienblad {
-  private ArrayList<Artikel> artikelen;
+    private ArrayList<Artikel> artikelen;
 
-  /**
-   * Constructor
-   */
-  public Dienblad() {
-    artikelen = new ArrayList<>(); 
-  }
+    /**
+     * Constructor
+     */
+    public Dienblad() {
+      artikelen = new ArrayList<>(); 
+    }
 
-  /**
-   * Methode om artikel aan dienblad toe te voegen
-   * @param artikel
-   */
-  public void voegToe(Artikel artikel) {
-      artikelen.add(artikel);
-  }
+    /**
+     * Methode om artikel aan dienblad toe te voegen
+     * @param artikel
+     */
+    public void voegToe(Artikel artikel) {
+        artikelen.add(artikel);
+    }
 
-  /**
-   * Methode om aantal artikelen op dienblad te tellen
-   * @return Het aantal artikelen 
-   */
-  public int getAantalArtikelen() {
-      return artikelen.size();
-  }
+    /**
+     * Methode om aantal artikelen op dienblad te tellen
+     * @return Het aantal artikelen 
+     */
+    public int getAantalArtikelen() {
+        return artikelen.size();
+    }
 
-  /**
-   * Methode om de totaalprijs van de artikelen 
-   * op dienblad uit te rekenen
-   * @return De totaalprijs
-   */
-  public void getTotaalPrijs() {
-      int totaal = 0;
-      
-      for(Artikel artikel : artikelen) {
-          totaal += artikel.getPrijs();
-      }
-      System.out.println(totaal);
-  }
+    /**
+     * Methode om de totaalprijs van de artikelen 
+     * op dienblad uit te rekenen
+     * @return De totaalprijs
+     */
+    public void getTotaalPrijs() {
+        int totaal = 0;
+
+        for(Artikel artikel : artikelen) {
+            totaal += artikel.getPrijs();
+        }
+        System.out.println(totaal);
+    }
 }
