3bc36ef716744a4d1332bd0f1b718e7e054ea85e
weetok <weetok@live.nl>
2014-11-28 12:00:31 +0100

    Implementatie Kassa

--- src/kantine/Kassa.java
+++ src/kantine/Kassa.java
@@ -1,49 +1,58 @@
 public class Kassa { 
-  /**
-   * Constructor
-   */
-  public Kassa(KassaRij kassarij) {
-    //method body omitted
-  }
-
-  /**
-   * vraag het aantal artikelen en de totaalprijs op.
-   * De implementatie wordt later vervangen 
-   * door een echte betaling door de persoon. 
-   * @param persoon die moet afrekenen
-   */
-  public void rekenAf(Persoon persoon) {
-    //method body omitted
-  }
-
-  /**
-   * Geeft het aantal artikelen dat de kassa 
-   * heeft gepasseerd, 
-   * vanaf het moment dat de methode resetWaarden 
-   * is aangeroepen.
-   * @return aantal artikelen
-   */
-  public int aantalArtikelen() {
-    //method body omitted
-  }
-
-  /**
-   * Geeft het totaalbedrag van alle artikelen die
-   * de kassa zijn gepasseerd, vanaf het moment dat de methode
-   *resetKassa 
-   * is aangeroepen.
-   * @return hoeveelheid geld in de kassa
-   */
-  public double hoeveelheidGeldInKassa() {
-    //method body omitted
-  }
-
-  /**
-   * reset de waarden van het aantal gepasseerde artikelen en 
-   * de totale hoeveelheid geld in de kassa.
-   */
-  public void resetKassa() {
-    //method body omitted
-  }
+	private Kassarij kassarij;
+	private int aantalGepasseerdeArtikelen;
+	private double totaalInKassa;
+	
+	/**
+	 * Constructor
+	 */
+	public Kassa(Kassarij kassarij) {
+		this.kassarij = kassarij;
+		aantalGepasseerdeArtikelen = 0;
+		totaalInKassa = 0.0;
+	}
+	
+	/**
+	 * vraag het aantal artikelen en de totaalprijs op.
+	 * De implementatie wordt later vervangen 
+	 * door een echte betaling door de persoon. 
+	 * @param persoon die moet afrekenen
+	 */
+	public void rekenAf(Persoon persoon) {
+		aantalGepasseerdeArtikelen += persoon.getAantalArtikelen();
+		totaalInKassa += persoon.getTotaalPrijs();
+		return persoon;
+	}
+	
+	/**
+	 * Geeft het aantal artikelen dat de kassa 
+	 * heeft gepasseerd, 
+	 * vanaf het moment dat de methode resetKassa 
+	 * is aangeroepen.
+	 * @return aantal artikelen
+	 */
+	public int aantalArtikelen() {
+		return aantalGepasseerdeArtikelen;
+	}
+	
+	/**
+	 * Geeft het totaalbedrag van alle artikelen die
+	 * de kassa zijn gepasseerd, vanaf het moment dat de methode
+	 *resetKassa 
+	 * is aangeroepen.
+	 * @return hoeveelheid geld in de kassa
+	 */
+	public double hoeveelheidGeldInKassa() {
+		return totaalInKassa;
+	}
+	
+	/**
+	 * reset de waarden van het aantal gepasseerde artikelen en 
+	 * de totale hoeveelheid geld in de kassa.
+	 */
+	public void resetKassa() {
+		aantalGepasseerdeArtikelen = 0;
+		totaalInKassa = 0;
+	}
 }
 
