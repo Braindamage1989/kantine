4c61af9f62a0103d1d28d0e9f9167b6565688d3b
Braindamage1989 <R.Scholten@Live.nl>
2014-12-02 10:50:08 +0100

    Verwijderen van aantalArtikelen(), hoeveelheidGeldInKassa(), resetKassa()

--- src/kantine/Kantine.java
+++ src/kantine/Kantine.java
@@ -11,6 +11,7 @@ package kantine;
 public class Kantine {
     private Kassa kassa;
     private Kassarij kassarij;
+    
 
   /**
    * Constructor
@@ -51,30 +52,5 @@ public class Kantine {
     while(kassarij.erIsEenRij()) {
         kassa.rekenAf(kassarij.eerstePersoonInRij());
     }
-  }
-
-  /**
-   * Deze methode telt het geld uit de kassa
-   * @return hoeveelheid geld in kassa
-   */
-  public double hoeveelheidGeldInKassa() {
-      return kassa.hoeveelheidGeldInKassa();
-  }
-
-  /**
-   * Deze methode geeft het aantal gepasseerde artikelen.
-   * @return het aantal gepasseerde artikelen
-   */
-  public int aantalArtikelen(){
-      return kassa.aantalArtikelen();
-  }
-
-  /**
-   * Deze methode reset de bijgehouden telling van 
-   * het aantal artikelen
-   * en "leegt" de inhoud van de kassa.
-   */
-  public void resetKassa() {
-      kassa.resetKassa();
   }
 }
