d1317fc0f3965d0ac5ffa0b80ed32c3f622414b7
Braindamage1989 <R.Scholten@Live.nl>
2014-12-11 15:51:01 +0100

    Opgave 1 week 4 bijna afgerond

--- src/kantine/Administratie.java
+++ src/kantine/Administratie.java
@@ -1,21 +1,47 @@
 public class Administratie {
-  /**
-   * Deze methode berekent van de int array aantal de 
-   * gemiddelde waarde
-   * @param aantal
-   * @return het gemiddelde
-   */
-  public double berekenGemiddeldAantal(int[] aantal) {
-    //omitted
-  }
+    /**
+    * Deze methode berekent van de int array aantal de 
+    * gemiddelde waarde
+    * @param aantal
+    * @return het gemiddelde
+    */
+    public static double berekenGemiddeldAantal(int[] aantal) {
+        double totaal = 0;
+        int deler = 0;
+        double gemiddeldAantal;
+        if(aantal.length>0) {
+            for(double aantallen : aantal) {
+                totaal += aantallen;
+                deler++;
+            }
+            gemiddeldAantal = totaal / deler;
+        }
+        else{
+            gemiddeldAantal = 0;
+        }   
+        return gemiddeldAantal;
+    }
 
-  /**
-   * Deze methode berekent van de double array omzet de 
-   * gemiddelde waarde
-   * @param omzet
-   * @return Het gemiddelde
-   */
-  public double berekenGemiddeldeOmzet(double[] omzet) {
-    //omitted
-  }
+    /**
+    * Deze methode berekent van de double array omzet de 
+    * gemiddelde waarde
+    * @param omzet
+    * @return Het gemiddelde
+    */
+    public static double berekenGemiddeldeOmzet(double[] omzet) {
+        double totaal = 0;
+        int deler = 0;
+        double gemiddeldeOmzet;
+        if(omzet.length>0) {
+            for(double dagomzet : omzet) {
+                totaal += dagomzet;
+                deler++;
+            }
+            gemiddeldeOmzet = totaal / deler;
+        }
+        else{
+            gemiddeldeOmzet = 0;
+        }
+        return gemiddeldeOmzet;
+    }
 }
