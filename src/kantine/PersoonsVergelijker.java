6b95bd14da0ea7bcba082755a3688523460c5332
Braindamage1989 <R.Scholten@Live.nl>
2014-12-29 13:20:32 +0100

    Persoonsvergelijker klasse aangemaakt

---
+++ src/kantine/PersoonsVergelijker.java
@@ -0,0 +1,18 @@
+package kantine;
+
+/**
+ * Maak 2 personen aan, en vergelijk deze
+ * @author Ronald Scholten
+ * @version 27-12-2014
+ */
+public class PersoonsVergelijker
+{
+    public static void main(String[] args)
+    {
+        Persoon persoon1 = new Persoon(335003, "Ronald", "Scholten", 1989, 12, 25, 'm');
+        Persoon persoon2 = new Persoon(335003, "Ronald", "Scholten", 1989, 12, 25, 'm');
+        
+        System.out.println(persoon1 == persoon2);
+        System.out.println(persoon1.equals(persoon2));
+    }
+}
