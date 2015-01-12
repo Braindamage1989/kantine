449f758bdf4245dbd2c8024de899c8bb8c6fbec7
Braindamage1989 <R.Scholten@Live.nl>
2015-01-12 11:58:15 +0100

    Slappe comments toegevoegd

--- src/kantine/Docent.java
+++ src/kantine/Docent.java
@@ -1,20 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kantine;
 
 /**
- *
- * @author R
- */
+* Een subklasse voor Persoon die alle
+* gegevens van docenten bevat.
+* 
+* @author Ronald Scholten & David Bor
+* @version 12-01-2014
+*/
 public class Docent extends Persoon implements KortingskaartHouder
 {
     private String afkorting;
     private String afdeling;
 
-
+    /**
+     * Constructor
+     */
     public Docent(int bsn, String voornaam, String achternaam, int geboortejaar, int geboortemaand, int geboortedag, char geslacht, String afkorting, String afdeling)
     {
         super(bsn, voornaam, achternaam, geboortejaar, geboortemaand, geboortedag, geslacht);
@@ -22,42 +22,65 @@ public class Docent extends Persoon implements KortingskaartHouder
         this.afdeling = afdeling;
     }
     
+    /**
+     * Een setter om de afdeling van een docent te setten
+     */
     public void setAfdeling(String afdeling)
     {
         this.afdeling = afdeling;
     }
     
+    /**
+     * Een setter om een afkorting van een docent te setten
+     */
     public void setAfkorting(String afkorting)
     {
         this.afkorting = afkorting;
     }
     
+    /**
+     * Een getter voor het kortingspercentage van een kantinemedewerker
+     */
     public double geefKortingsPercentage()
     {
         return 0.25;
     }
 
+    /**
+     * Een getter om te bepalen of er een maximum is
+     */
     public boolean heeftMaximum()
     {
         return true;
     }
 
+    /**
+     * Een getter die het maximum terug geeft, 0.0 betekend geen limiet
+     */
     public double geefMaximum()
     {
         return 1.0;
     }
 
-    
+    /**
+     * Een getter die de afdeling ophaalt
+     */
     public String getAfdeling()
     {
         return afdeling;
     }
     
+    /**
+     * Een getter die een afkorting van de docent geeft
+     */
     public String getAfkorting()
     {
         return afkorting;
     }
     
+    /**
+     * Override van toString() voor docent
+     */
     public String toString()
     {
         return super.toString() +
--- src/kantine/Student.java
+++ src/kantine/Student.java
@@ -1,20 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kantine;
 
 /**
- *
- * @author R
- */
+* Een subklasse voor Persoon die alle
+* gegevens van studenten bevat.
+* 
+* @author Ronald Scholten & David Bor
+* @version 12-01-2014
+*/
 public class Student extends Persoon
 {
     private String studentnummer;
     private String studierichting;
 
-
+    /**
+    * Constructor
+    */
     public Student(int bsn, String voornaam, String achternaam, int geboortejaar, int geboortemaand, int geboortedag, char geslacht, String studentnummer, String studierichting)
     {
         super(bsn, voornaam, achternaam, geboortejaar, geboortemaand, geboortedag, geslacht);
@@ -22,26 +22,41 @@ public class Student extends Persoon
         this.studierichting = studierichting;
     }
     
+    /**
+     * Setter om een studentnummer te setten
+     */
     public void setStudentnummer(String studentnummer)
     {
         this.studentnummer = studentnummer;
     }
     
+    /**
+     * Setter om een studierichting aan te geven
+     */
     public void setStudierichting(String studierichting)
     {
         this.studierichting = studierichting;
     }
     
+    /**
+     * Getter voor het verkrijgen van een studentnummer
+     */
     public String getStudentnummer()
     {
         return studentnummer;
     }
     
+    /**
+     * Getter voor het verkrijgen van een studierichting
+     */
     public String getStudierichting()
     {
         return studierichting;
     }
     
+    /**
+     * Override van toString() voor docent
+     */
     public String toString()
     {
         return super.toString() +
